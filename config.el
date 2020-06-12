92d46965a4b3bf581b63ee0417c3c7f5b61e025a
Christabella Irwanto <christabellai@spotify.com>
2020-06-12 16:55:42 +0300

    Start in full-screen. Thanks @dubnev.
    https://github.com/dubnev/.doom.d/blob/12dbe011bff21aabd9614f3dc1cbefd6dcff750a/config.el

--- config.el
+++ config.el
@@ -3,6 +3,8 @@
 ;; Place your private configuration here! Remember, you do not need to run 'doom
 ;; sync' after modifying this file!
 
+;; Start in full screen by default
+(toggle-frame-maximized)
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
