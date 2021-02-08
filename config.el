0eed3cc70e333d06b2df4f858882ee5e55c6fb20
Christabella Irwanto <cirwanto@indeed.com>
2021-02-08 17:55:12 +0800

    Turn off line nums (slows down multiple-cursors).

--- config.el
+++ config.el
@@ -40,7 +40,7 @@
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type t)
+(setq display-line-numbers-type nil)
 
 ;; Autofill comments
 (setq comment-auto-fill-only-comments t)
