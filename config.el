e43b01e207fe8db82da5922746f87f4dd90515d7
Christabella Irwanto <christabella.irwanto@relexsolutions.com>
2025-12-08 13:53:23 +0200

    Update config: change email, switch to doom-fairy-floss theme, update org-journal keybindings

--- config.el
+++ config.el
@@ -10,7 +10,7 @@
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
 (setq user-full-name "Christabella Irwanto"
-      user-mail-address "christabella.irwanto@gmail.com")
+      user-mail-address "christabella.irwanto@relexsolutions.com")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
 ;; are the three important ones:
@@ -34,8 +34,8 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. I like these themes:
-(setq doom-theme 'doom-laserwave)
-;; (setq doom-theme 'doom-fairy-floss)
+;; (setq doom-theme 'doom-laserwave)
+(setq doom-theme 'doom-fairy-floss)
 ;; (setq doom-theme 'doom-dracula)
 ;; (setq doom-theme 'doom-moonlight)
 
@@ -189,10 +189,10 @@
   (map!
    "s-j" #'org-journal-new-entry
    "C-M-S-s-j" #'org-journal-new-entry
-   "C-c C-b" #'org-journal-open-previous-entry
-   "C-c C-f" #'org-journal-open-next-entry
-   "C-M-S-s-k" #'org-journal-open-current-journal-file  ;; Today, or Kyō (今日)
-   "s-J" #'org-journal-open-current-journal-file))
+   "C-c C-b" #'org-journal-previous-entry
+   "C-c C-f" #'org-journal-next-entry
+   "s-J" #'org-journal-open-current-journal-file
+   ))
 
 (use-package! org-download
   :commands
@@ -301,15 +301,15 @@
            :target (file+head "lit/${slug}.org"
                               "#+setupfile:./hugo_setup.org\n#+hugo_slug: ${slug}\n#+title: ${title}\n")
            :unnarrowed t)
-        ("c" "concept" plain "%?"
+          ("c" "concept" plain "%?"
            :target (file+head "concepts/${slug}.org"
                               "#+setupfile:./hugo_setup.org\n#+hugo_slug: ${slug}\n#+title: ${title}\n")
            :unnarrowed t)
-        ("p" "personal" plain "%?"
+          ("p" "personal" plain "%?"
            :target (file+head "personal/${slug}.org"
                               "#+title: ${title} (%<%Y-%m-%d>)\n")
            :unnarrowed t))
-  ))
+        ))
 
 (use-package treemacs-icons-dired
   :after treemacs dired
@@ -337,4 +337,79 @@
     (format-all-mode -1)))
 
 (use-package emacsql-sqlite3)
-(setq org-roam-database-connector 'sqlite3)
+(setq org-roam-database-connector 'sqlite3);;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets. It is optional.
+;; (setq user-full-name "John Doe"
+;;       user-mail-address "john@doe.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom:
+;;
+;; - `doom-font' -- the primary font to use
+;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
+;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;; - `doom-symbol-font' -- for symbols
+;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
+;;
+;; See 'C-h v doom-font' for documentation and more examples of what they
+;; accept. For example:
+;;
+;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
+;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
+;;
+;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
+;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
+;; refresh your font settings. If Emacs still can't find your font, it likely
+;; wasn't installed correctly. Font issues are rarely Doom issues!
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-one)
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type t)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/org/")
+
+
+;; Whenever you reconfigure a package, make sure to wrap your config in an
+;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
+;;
+;;   (after! PACKAGE
+;;     (setq x y))
+;;
+;; The exceptions to this rule:
+;;
+;;   - Setting file/directory variables (like `org-directory')
+;;   - Setting variables which explicitly tell you to set them before their
+;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
+;;   - Setting doom variables (which start with 'doom-' or '+').
+;;
+;; Here are some additional functions/macros that will help you configure Doom.
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
+;; etc).
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
